2928dd824fb8161351cf4c2bf0d071870b769e8a
sagi.yin@gmail.com <sagi.yin@gmail.com@71db1383-f731-0250-efd6-fa40924ff3df>
2011-12-05 21:26:53 +0000

    Vanilla VOIP Client

---
+++ trunk/src/edu/purdue/cs/voip/client/VOIP_ClientActivity.java
@@ -0,0 +1,13 @@
+package edu.purdue.cs.voip.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VOIP_ClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
