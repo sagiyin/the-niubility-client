bb1e976ff5272e527fc64609507d5fcc5eaaac66
michaelrobtemp@gmail.com <michaelrobtemp@gmail.com@71db1383-f731-0250-efd6-fa40924ff3df>
2011-12-07 02:37:05 +0000

    Call and End Call Switch success!

---
+++ src/edu/purdue/cs/voip/client/OutgoingCall.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package edu.purdue.cs.voip.client;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * @author lzhen
+ * 
+ */
+public class OutgoingCall extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    final OutgoingCall self = this;
+    setContentView(R.layout.outgoing);
+
+    TextView textView = (TextView) findViewById(R.id.label);
+    textView.setText(VOIP_ClientActivity.getCurrentIP());
+
+    Button endCall = (Button) findViewById(R.id.endCall);
+
+    endCall.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        startActivity(new Intent(self, VOIP_ClientActivity.class));
+      }
+    });
+
+  }
+
+}
--- src/edu/purdue/cs/voip/client/VOIP_ClientActivity.java
+++ src/edu/purdue/cs/voip/client/VOIP_ClientActivity.java
@@ -1,13 +1,60 @@
 package edu.purdue.cs.voip.client;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class VOIP_ClientActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class VOIP_ClientActivity extends ListActivity {
+  /** Called when the activity is first created. */
+  // get current online user here
+  private String[] onlineUsers = { "lzhen", "SG", "BB" };
+  private static String currentIP = null;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    final VOIP_ClientActivity self = this;
+    setListAdapter(new ArrayAdapter<String>(this, R.layout.main, onlineUsers));
+
+    ListView lv = getListView();
+    lv.setTextFilterEnabled(true);
+
+    lv.setOnItemClickListener(new OnItemClickListener() {
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        startActivity(new Intent(self, OutgoingCall.class));
+        
+        currentIP = (String) ((TextView) view).getText();
+
+        // When clicked, show a toast with the TextView text
+        // Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+        // Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+  
+  public static String getCurrentIP(){
+    return currentIP;
+  }
+
+  // @Override
+  // public void onCreate(Bundle savedInstanceState) {
+  // super.onCreate(savedInstanceState);
+  // final VOIP_ClientActivity self = this;
+  // Button goTo = new Button(this);
+  //
+  // goTo.setText("goTo");
+  // goTo.setOnClickListener(new View.OnClickListener() {
+  // public void onClick(View v) {
+  // startActivity(new Intent(self, TestPage.class));
+  // }
+  // });
+  // setContentView(R.layout.main);
+  // }
+
 }
