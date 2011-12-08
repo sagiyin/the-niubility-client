f4fa8898625422cd471940c9b0e9b7794dc65a92
jshanggu@gmail.com <jshanggu@gmail.com@71db1383-f731-0250-efd6-fa40924ff3df>
2011-12-08 02:13:14 +0000

    update

--- src/edu/purdue/cs/voip/client/OnlineList.java
+++ src/edu/purdue/cs/voip/client/OnlineList.java
@@ -10,33 +10,37 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 /**
  * @author lzhen
- *
+ * 
  */
 public class OnlineList extends ListActivity {
-  
-//get current online user here
+
+  // get current online user here
   private String[] onlineUsers = { "lzhen", "SG", "BB" };
-  private static String currentIP = null;
+  private static String currentIP = "temp";
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.onlinelist);
     final OnlineList self = this;
-    setListAdapter(new ArrayAdapter<String>(this, R.layout.onlinelist, onlineUsers));
+    setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, onlineUsers));
 
-    ListView lv = getListView();
+    // Button endCall = (Button) findViewById(R.id.endCall);
+
+    //ListView lv = getListView();
+    final ListView lv = (ListView) findViewById(android.R.id.list);
     lv.setTextFilterEnabled(true);
-
     lv.setOnItemClickListener(new OnItemClickListener() {
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         startActivity(new Intent(self, OutgoingCall.class));
-        
-        currentIP = (String) ((TextView) view).getText();
+
+        currentIP = lv.getSelectedItem().toString();
 
         // When clicked, show a toast with the TextView text
         // Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
@@ -44,8 +48,8 @@ public class OnlineList extends ListActivity {
       }
     });
   }
-  
-  public static String getCurrentIP(){
+
+  public static String getCurrentIP() {
     return currentIP;
   }
 
