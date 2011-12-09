fca65e383a0ce61a32abba17d0b7f7d9019d1a3e
sagi.yin@gmail.com <sagi.yin@gmail.com@71db1383-f731-0250-efd6-fa40924ff3df>
2011-12-09 01:37:58 +0000

    UDP based AudioReceiver/AudioSender

---
+++ trunk/src/edu/purdue/cs/voip/client/AudioSender.java
@@ -0,0 +1,68 @@
+package edu.purdue.cs.voip.client;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+public class AudioSender extends Thread {
+  VOIP_ClientActivity clientActivity;
+  DatagramSocket socket;
+  DatagramPacket packet;
+  int buf_size = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO,
+      AudioFormat.ENCODING_PCM_16BIT);
+  AudioRecord record;
+  boolean recording;
+  InetAddress targetIP;
+  int targetPort;
+  
+  public AudioSender(VOIP_ClientActivity clientActivity, String targetIP, int targetPort) {
+    this.clientActivity = clientActivity;
+    this.recording = false;
+    try {
+      this.targetIP = InetAddress.getByName(targetIP);
+      this.targetPort = targetPort;
+      socket = new DatagramSocket();
+    } catch (SocketException e) {
+      e.printStackTrace();
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void startRecording() {
+    record = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100, AudioFormat.CHANNEL_IN_MONO,
+        AudioFormat.ENCODING_PCM_16BIT, buf_size);
+    record.startRecording();
+    this.recording = true;
+  }
+
+  public void stopRecording() {
+    this.recording = false;
+    record.stop();
+    record.release();
+  }
+
+  public void run() {
+    while (true) {
+      if (recording) {
+        byte[] buffer = new byte[256];
+        record.read(buffer, 0, buffer.length);
+        
+        packet = new DatagramPacket(buffer, buffer.length, targetIP, targetPort);
+        
+        try {
+          socket.send(packet);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
